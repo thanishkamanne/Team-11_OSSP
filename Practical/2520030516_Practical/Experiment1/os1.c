ef6fae5f918714d9a511e355ce026c4ca24a8ccd
thanishkamanne <thanishkamanne@gmail.com>
2026-08-04 08:33:59 +0530

    Add files via upload

---
+++ Practical/2520030516_Practical/Experiment1/os1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+fork();
+printf("Hello\n");
+return 0;
+}
+
+
---
+++ Practical/2520030516_Practical/Experiment1/os8 (1).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+int i;
+for(i=0;i<3;i++)
+fork();
+printf("PID = %d\n",getpid());
+return 0;
+}
+
+
+
+
+
