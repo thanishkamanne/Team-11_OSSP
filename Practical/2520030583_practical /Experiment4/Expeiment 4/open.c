99417111588f51fd963b9c3388f59bfe9aaf429c
thanishkamanne <thanishkamanne@gmail.com>
2026-08-18 09:40:45 +0530

    Add files via upload

---
+++ Practical/2520030583_practical /Experiment4/Expeiment 4/open.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+int fd;
+fd=open("input.txt",O_RDONLY);
+if(fd==-1)
+{
+perror("Error opening file");
+return 1;
+}
+printf("File opened succesfully.\n");
+printf("File descriptor:%d\n",fd);
+close(fd);
+return 0;
+}
