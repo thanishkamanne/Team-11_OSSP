56102716e0e83cbe0411818c8d6a6464104168db
joshirushithakasturi-arch <joshirushithakasturi@gmail.com>
2026-09-21 08:38:58 +0530

    Add files via upload

---
+++ Skill/2520030204_Skill/skill5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char *name = "Rushitha";
+
+    printf("Single quote example: '$name'\n");
+    printf("Double quote example: \"%s\"\n", name);
+
+    return 0;
+}
